8111e707ad0a70871c070b29b816d9f0ef590367
Sarah Rainsberger <5098874+sarah11918@users.noreply.github.com>
2021-12-22 14:19:55 -0400

    some files to test GraphQL api call in Astro

---
+++ src/components/experiments/api.js
@@ -0,0 +1,31 @@
+async function fetchApi(query, {variables} = {}) {
+    const headers = {'Content-Type':'application/json'};
+    const response = await fetch("https://graphql-weather-api.herokuapp.com", {
+        method:'POST',
+        headers,
+        body:JSON.stringify({query, variables}),
+    });
+    const json = await response.json();
+    if(json.errors){
+        console.log(json.errors);
+        throw new Error('Failed to fetch API');
+    }
+    return json.data;
+}
+
+export async function getWeather(){
+    const data= await fetchApi(`
+    {
+        getCityByName(name: "Toronto"){
+            name
+            country
+            weather {
+                summary {
+                    description
+                }
+            }
+        }
+    }
+    `)
+    return data
+}
