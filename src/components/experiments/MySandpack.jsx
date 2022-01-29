0008209f8b4a16a3f414b64eb6647781543a71f2
Sarah Rainsberger <sarah@rainsberger.ca>
2022-01-29 16:32:09 +0000

    Sandpack embedded playground working!

---
+++ src/components/experiments/MySandpack.jsx
@@ -0,0 +1,13 @@
+
+import { Sandpack } from "@codesandbox/sandpack-react";
+import "@codesandbox/sandpack-react/dist/index.css";
+
+function MySandpack() {
+  return (
+    <>
+      <h1>Code Playground</h1>
+      <Sandpack template="react" />
+    </>
+  )
+}
+export default MySandpack;
